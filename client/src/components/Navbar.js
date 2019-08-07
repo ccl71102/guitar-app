aff5138b031de0b59bb4842898b597384a02f60c
ccl71102 <colonlopezcarlos@gmail.com>
2019-08-06 21:40:32 -0600

    added icons everywhere

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMusic, faGuitar, faListAlt} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
         <div>
-            <Link to="/tools">Tools</Link>
-            <Link to="/tabs">Tabs</Link>
-            <Link to="/credits">Credits</Link>
+            <Link to="/tools"><FontAwesomeIcon icon={faGuitar}/> Tools</Link>
+            <Link to="/tabs"><FontAwesomeIcon icon={faMusic}/> Tabs</Link>
+            <Link to="/credits"><FontAwesomeIcon icon={faListAlt}/> Credits</Link>
         </div>
     )
 }
--- client/src/components/toolComponents/Metronome.js
+++ client/src/components/toolComponents/Metronome.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusCircle, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
+import { faPlusCircle, faMinusCircle, faPlayCircle, faStopCircle } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 
@@ -13,13 +13,13 @@ const Metronome = props => {
         <select onChange={handleChange} name="metronome" defaultValue={metronome}>
             <option value="Standard">Standard</option>
             <option value="Basic Beat">Basic Beat</option>
-            <option value="Blast Beats">Blast Beats</option>
+            {/* <option value="Blast Beats">Blast Beats</option> */}
         </select>
         <p>BPM: </p>
         <input onChange={props.handleChange} readOnly type="number" name="bpm" value={bpm} min="1" max="220"/>
         <FontAwesomeIcon onClick={() => changeBPM(1)} icon={faPlusCircle}/>
         <FontAwesomeIcon onClick={() => changeBPM(-1)} icon={faMinusCircle}/>
-        {!isPlaying ? <button onClick={playMetronome}>Play</button> : <button onClick={stopMetronome}>Stop</button>}
+        {!isPlaying ? <FontAwesomeIcon icon={faPlayCircle} onClick={playMetronome}/> : <FontAwesomeIcon icon={faStopCircle} onClick={stopMetronome}/>}
     </div>
     );
 }
