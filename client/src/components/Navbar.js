188511e9274835b62ee94dc3fd19cb0f958402b8
ccl71102 <colonlopezcarlos@gmail.com>
2019-08-14 16:14:05 -0600

    navbar highlights current page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { withRouter } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic, faGuitar, faListAlt} from "@fortawesome/free-solid-svg-icons";
 
-const Navbar = () => {
+const Navbar = props => {
     
     return (
         <div id="navbar">
-            <NavLink to="/tools" activeClassName="active"><FontAwesomeIcon icon={faGuitar}/> Tools</NavLink>
-            <NavLink to="/tabs" activeClassName="active"><FontAwesomeIcon icon={faMusic}/> Tabs</NavLink>
-            <NavLink to="/credits" activeClassName="active"><FontAwesomeIcon icon={faListAlt}/> Credits</NavLink>
+            <NavLink to="/tools" className="navlink" activeClassName={props.location.pathname === "/tools" ? "active" : ""}><FontAwesomeIcon icon={faGuitar}/> Tools</NavLink>
+            <NavLink to="/tabs" className="navlink" activeClassName={(props.location.pathname === "/tabs" || props.location.pathname === "/auth") ? "active" : ""}><FontAwesomeIcon icon={faMusic}/> Tabs</NavLink>
+            <NavLink to="/credits" className="navlink" activeClassName={props.location.pathname === "/credits" ? "active" : ""}><FontAwesomeIcon icon={faListAlt}/> Credits</NavLink>
         </div>
     );
 }
 
-export default Navbar;
+export default withRouter(Navbar);
